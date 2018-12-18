55cd6827fd2640705a8febd6c75e4306130d3e32
sudnonk <sudnonk12@yahoo.co.jp>
2018-12-18 12:25:03 +0900

    初期化処理を書いてベンチマーク取った

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
-package go_mas
+package main
+
+import (
+	"github.com/sudnonk/go_mas/models"
+	"log"
+	"time"
+)
 
 func main() {
+	ss := int64(0)
+	log.Println("start")
+	for i := 0; i < 10000; i++ {
+		var u models.Universe
+		s := time.Now()
+		u.Init()
+		ss += time.Since(s).Nanoseconds()
+	}
 
+	log.Println("end")
+	log.Println(ss / 10000)
 }
 
-func init() {
-
+func world() {
+	var u models.Universe
+	u.Init()
 }
--- models/Universe.go
+++ models/Universe.go
@@ -1,6 +1,8 @@
 package models
 
-import "github.com/sudnonk/go_mas/config"
+import (
+	"github.com/sudnonk/go_mas/config"
+)
 
 type Universe struct {
 	Agents map[int64]Agent
@@ -8,11 +10,24 @@ type Universe struct {
 
 func (u *Universe) Init() {
 	var i int64
+
+	u.Agents = map[int64]Agent{}
+
 	for i = 0; i < config.MaxAgents; i++ {
 		u.Agents[i] = NewAgent(i)
 	}
+
+	u.makeNetwork()
 }
 
-func (u *Universe) MakeNetwork() {
+func (u *Universe) makeNetwork() {
 	//todo: 実装
+}
+
+func (u *Universe) step() {
+	for _, a := range u.Agents {
+		for _, a2 := range u.Agents {
+
+		}
+	}
 }
