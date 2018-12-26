a97c49fa256333070db7e986d0f89bf857ccb519
sudnonk <sudnonk12@yahoo.co.jp>
2018-12-27 08:13:46 +0900

    並列実行数を増やす

--- config/Config.go
+++ config/Config.go
@@ -1,13 +1,13 @@
 package config
 
 const (
-	MaxUniverse = 100
+	MaxUniverse = 10000
 	MaxAgents   = 1000
 	MaxHP       = 1000
 	MaxIdeology = 10000
 	MaxRecovery = 100
 
-	MaxSteps = 10000
+	MaxSteps = 1000
 
 	NearCriteria = 0.2
 	FarCriteria  = 0.8
