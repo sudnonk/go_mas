6882a5d46910e28e455a48059388d0ceeafed323
sudnonk <sudnonk12@yahoo.co.jp>
2018-12-27 08:05:48 +0900

    並列で動かす

--- main.go
+++ main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
+	"github.com/sudnonk/go_mas/config"
 	"github.com/sudnonk/go_mas/models"
 	"log"
-	"time"
 )
 
 func main() {
-	ss := int64(0)
-	log.Println("start")
-	for i := 0; i < 10000; i++ {
-		var u models.Universe
-		s := time.Now()
-		u.Init()
-		ss += time.Since(s).Nanoseconds()
+	cs := make(chan struct{})
+	for i := 0; i < config.MaxUniverse; i++ {
+		go world(cs)
 	}
-
-	log.Println("end")
-	log.Println(ss / 10000)
+	<-cs
 }
 
-func world() {
+func world(c chan struct{}) {
+	log.Println("called")
 	var u models.Universe
 	u.Init()
+	for i := 0; i < config.MaxSteps; i++ {
+		log.Println("step" + string(i))
+		u.Step()
+	}
+	u.End()
+	log.Println("end")
+	c <- struct{}{}
 }
